2191c8b26d23df764d9e16ce8c2342bc89fe660e
sophielou987 <sophie.lou.95@hotmail.co.uk>
2019-06-11 19:59:18 +0100

    Heroku setup

--- send_message.py
+++ send_message.py
@@ -4,8 +4,8 @@ import os
 load_dotenv() # This will load .env file as enviroment variables allowing you to continue developing locally as before
 
 def send_message(email, message):
-  mail_api_key= os.getenv("MAILGUN_API_KEY") # This will read necessary variables from enviroment variables
-  mail_domain = os.getenv("MAILGUN_API_DOMAIN")
+  mail_api_key= os.getenv("MAILGUN_API") # This will read necessary variables from enviroment variables
+  mail_domain = os.getenv("MAILGUN_DOMAIN")
 
   return requests.post(
     "https://api.mailgun.net/v3/{}/messages".format(mail_domain),
