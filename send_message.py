b9d29ff8024bc02538fe99be0e35a1e85e889621
sophielou987 <sophie.lou.95@hotmail.co.uk>
2019-06-11 19:48:49 +0100

    changing the message file

--- send_message.py
+++ send_message.py
@@ -1,10 +1,11 @@
 import requests
-from dotenv import dotenv_values
-mail_configuration = dotenv_values()
+from dotenv import load_dotenv
+import os
+load_dotenv() # This will load .env file as enviroment variables allowing you to continue developing locally as before
 
 def send_message(email, message):
-  mail_api_key= mail_configuration["MAILGUN_API_KEY"]
-  mail_domain = mail_configuration["MAILGUN_API_DOMAIN"]
+  mail_api_key= os.getenv("MAILGUN_API_KEY") # This will read necessary variables from enviroment variables
+  mail_domain = os.getenv("MAILGUN_API_DOMAIN")
 
   return requests.post(
     "https://api.mailgun.net/v3/{}/messages".format(mail_domain),
